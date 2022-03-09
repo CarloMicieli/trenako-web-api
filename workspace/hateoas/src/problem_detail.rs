979b3e0555b4d346525caccb0a15151fdf7ac64a
CarloMicieli <carlo.micieli@gmail.com>
2022-03-09 21:31:24 +0100

    feat(webapi): add the problem detail struct

---
+++ workspace/hateoas/src/problem_detail.rs
@@ -0,0 +1,76 @@
+use std::collections::BTreeMap;
+
+use crate::urn::Urn;
+use chrono::{DateTime, Utc};
+use derive_builder::Builder;
+use serde::Serialize;
+use validator::ValidationErrors;
+
+/// From RFC-7807
+/// "problem detail" is a way to carry machine-readable details of errors in a HTTP response to avoid
+/// the need to define new error response formats for HTTP APIs.
+#[derive(Debug, Serialize, Builder)]
+pub struct ProblemDetail {
+    problem_type: Urn,
+    timestamp: DateTime<Utc>,
+    title: String,
+    detail: String,
+    fields: std::collections::BTreeMap<String, Vec<String>>,
+    status_code: u16,
+    instance: Urn,
+}
+
+impl ProblemDetail {
+    pub fn new(problem_id: uuid::Uuid) -> ProblemDetail {
+        ProblemDetailBuilder::default()
+            .instance(Urn::instance(&problem_id))
+            .problem_type(Urn::error())
+            .title(String::from("Internal Server Error"))
+            .build()
+            .unwrap()
+    }
+
+    pub fn from_error(error: &str) -> ProblemDetail {
+        let now = chrono::Utc::now();
+        let id = uuid::Uuid::new_v4();
+        ProblemDetail {
+            problem_type: Urn::error(),
+            timestamp: now,
+            title: String::from("Internal Server Error"),
+            detail: error.to_owned(),
+            fields: BTreeMap::new(),
+            status_code: http::StatusCode::INTERNAL_SERVER_ERROR.as_u16(),
+            instance: Urn::instance(&id),
+        }
+    }
+
+    /// Creates a new Uniform Resource Name (URN) for an invalid request
+    pub fn from_invalid_request(
+        entity: &str,
+        validation_errors: ValidationErrors,
+    ) -> ProblemDetail {
+        let now = chrono::Utc::now();
+        let id = uuid::Uuid::new_v4();
+
+        let mut fields = BTreeMap::new();
+        for (field, errors) in validation_errors.field_errors() {
+            let value = errors
+                .iter()
+                .map(|e| e.to_string())
+                .collect::<Vec<String>>();
+            fields.insert(field.to_owned(), value);
+        }
+
+        ProblemDetail {
+            problem_type: Urn::invalid_request(entity),
+            timestamp: now,
+            title: String::from("Invalid request"),
+            detail: String::from(
+                "Fields validation failed for this request. Check them before you try again.",
+            ),
+            fields,
+            status_code: http::StatusCode::BAD_REQUEST.as_u16(),
+            instance: Urn::from_uuid(entity, &id),
+        }
+    }
+}
